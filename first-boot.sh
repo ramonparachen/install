c0ff76c2f68c862303abc67d900e73c1c75ad8d9
Ramon <ramonparachen@gmail.com>
2022-07-03 15:52:59 -0300

    Add line to remap caps lock to esc

--- first-boot.sh
+++ first-boot.sh
@@ -3,3 +3,5 @@ sudo timedatectl set-ntp true
 sudo pacman -S xorg lightdm lightdm-gtk-greeter awesome firefox vlc qbittorrent vlc flatpak
 
 systemctl enable lightdm.service
+
+setxkbmap -option caps:escape_shifted_capslock
