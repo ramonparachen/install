87b973fb27fb25cb9c21c1a56bf8352784ca3017
Ramon <ramonparachen@gmail.com>
2022-07-05 14:58:43 -0300

    Added xterm to first-boot.sh

--- first-boot.sh
+++ first-boot.sh
@@ -1,6 +1,6 @@
 #wm + software
 sudo timedatectl set-ntp true
-sudo pacman -S xorg lightdm lightdm-gtk-greeter awesome firefox vlc qbittorrent vlc flatpak
+sudo pacman -S xorg xterm lightdm lightdm-gtk-greeter awesome firefox vlc qbittorrent vlc flatpak
 
 systemctl enable lightdm.service
 
