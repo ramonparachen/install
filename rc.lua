995ab3a69821748443088f1dccfca15d9693bd8e
Ramon Parachen <ramonparachen@gmail.com>
2022-07-18 15:40:16 -0300

    Changed layoutbox position

--- rc.lua
+++ rc.lua
@@ -194,6 +194,7 @@ awful.screen.connect_for_each_screen(function(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
+            s.mylayoutbox,
             s.mytaglist,
             s.mypromptbox,
         },
@@ -201,7 +202,6 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
         },
     }
 end)
