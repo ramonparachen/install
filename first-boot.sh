adcecd73082c8823a5effcb01e99d3f65b562601
Ramon Parachen <ramonparachen@gmail.com>
2022-07-27 16:36:04 -0300

    Removed lines related to keyboard settings, now is handled on chroot-custom.sh file

--- first-boot.sh
+++ first-boot.sh
@@ -3,5 +3,3 @@ sudo timedatectl set-ntp true
 sudo pacman -S xorg xterm alacritty lightdm lightdm-gtk-greeter awesome network-manager-applet firefox vlc qbittorrent vlc flatpak
 
 systemctl enable lightdm.service
-
-setxkbmap -option caps:escape_shifted_capslock
