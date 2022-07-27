2d3e6d35a1431b241f397f4dff1fd04c2904aa0f
Ramon Parachen <ramonparachen@gmail.com>
2022-07-27 16:44:58 -0300

    Added lines to setup AwesomeWM files correctly

--- first-boot.sh
+++ first-boot.sh
@@ -3,3 +3,7 @@ sudo timedatectl set-ntp true
 sudo pacman -S xorg xterm alacritty lightdm lightdm-gtk-greeter awesome network-manager-applet firefox vlc qbittorrent vlc flatpak
 
 systemctl enable lightdm.service
+
+rm -rf /home/ramon/.config/awesome
+cp -r /install/awesome /home/ramon/.config/
+chmod +x /home/ramon/.config/awesome/autorun.sh
