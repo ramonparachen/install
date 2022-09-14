c2836ac10eb097d20ef73f23d0239a25b684257f
Ramon <ramonparachen@gmail.com>
2022-09-14 17:15:27 -0300

    Removed duplicated vlc entry

--- first-boot.sh
+++ first-boot.sh
@@ -1,6 +1,6 @@
 #wm + software
 sudo timedatectl set-ntp true
-sudo pacman -S xorg xterm alacritty lightdm lightdm-gtk-greeter awesome network-manager-applet firefox vlc qbittorrent vlc flatpak
+sudo pacman -S xorg xterm alacritty lightdm lightdm-gtk-greeter awesome network-manager-applet firefox vlc qbittorrent flatpak
 
 systemctl enable lightdm.service
 
