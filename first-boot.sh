5ef22814927b230dcbc780598553ae2933f06418
Ramon Parachen <ramonparachen@gmail.com>
2022-10-19 18:31:35 -0300

    Moved localectl set-x11-keymap to first-boot.sh file

--- first-boot.sh
+++ first-boot.sh
@@ -4,6 +4,9 @@ sudo pacman -S xorg xterm kitty lightdm lightdm-gtk-greeter awesome network-mana
 
 systemctl enable lightdm.service
 
+#Keyboard settings
+localectl --no-convert set-x11-keymap "us,us" "pc86" "us-intl" "caps:escape_shifted_capslock"
+
 cp -r /install/.config /home/ramon
 chmod +x /home/ramon/.config/awesome/autorun.sh
 cp /install/.vimrc /home/ramon
